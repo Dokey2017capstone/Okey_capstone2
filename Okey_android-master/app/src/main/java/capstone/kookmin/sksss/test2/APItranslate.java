6428d5fc1b235f5d161841c2345b1ee234e1d31c
noble003 <noble003@nate.com>
2017-12-07 21:51:03 +0900

    return string

--- Okey_android-master/app/src/main/java/capstone/kookmin/sksss/test2/APItranslate.java
+++ Okey_android-master/app/src/main/java/capstone/kookmin/sksss/test2/APItranslate.java
@@ -1,9 +1,5 @@
 package capstone.kookmin.sksss.test2;
 
-import android.util.Log;
-import android.view.inputmethod.ExtractedTextRequest;
-import android.view.inputmethod.InputConnection;
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
@@ -17,10 +13,8 @@ import java.net.URLEncoder;
 
 // 네이버 Papago NMT API
 
-public class APItranslate{
-
-
-    public static String APItranslate(CharSequence inputtext,String sourcetext,String targettext) {
+public class APItranslate {
+    public static String APItranslate(CharSequence inputtext, String sourcetext, String targettext) {
 
         String clientId = "18WZck0vzX6RMMjqLajD";//애플리케이션 클라이언트 아이디값";
         String clientSecret = "K6I8tDaj75";//애플리케이션 클라이언트 시크릿값";
@@ -30,12 +24,12 @@ public class APItranslate{
             String text = URLEncoder.encode(inputtext.toString(), "UTF-8");
             String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
             URL url = new URL(apiURL);
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("POST");
             con.setRequestProperty("X-Naver-Client-Id", clientId);
             con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
             // post request
-            String postParams = "source="+sourcetext+"&"+"target="+targettext+"&text=" + text;
+            String postParams = "source=" + sourcetext + "&" + "target=" + targettext + "&text=" + text;
             con.setDoOutput(true);
             DataOutputStream wr = new DataOutputStream(con.getOutputStream());
             wr.writeBytes(postParams);
@@ -43,7 +37,7 @@ public class APItranslate{
             wr.close();
             int responseCode = con.getResponseCode();
             BufferedReader br;
-            if(responseCode==200) { // 정상 호출
+            if (responseCode == 200) { // 정상 호출
                 br = new BufferedReader(new InputStreamReader(con.getInputStream()));
             } else {  // 에러 발생
                 br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
@@ -54,10 +48,20 @@ public class APItranslate{
                 response.append(inputLine);
             }
             br.close();
-            return (response.toString());
+
+
+            String tempoutput=response.toString();
+            String output="";
+            int index=tempoutput.indexOf("translatedText")+17;
+
+            output=tempoutput.substring(index,tempoutput.length()-4);
+
+            return output;
         } catch (Exception e) {
             System.out.println(e);
+            return "";
         }
     }
-}
 
+
+}
